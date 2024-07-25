7a472af48a389b8a207848912e3cf2c1c64e4821
Matthew Navy <128771121+Saratarr@users.noreply.github.com>
2024-07-25 18:06:16 -0500

    init

---
+++ NameAgeColor.py
@@ -0,0 +1,25 @@
+# CEIS150 - Module 1 Discussion - Topic 2
+# "Write a python program to accept a user's name, age, and favorite color and
+# print out a welcome message."
+# Matthew Navy
+# July 3 2023
+
+user_name = input("Greetings, user. What is your name? ")
+print()
+
+print("We always welcome new users,",user_name,"!")
+print()
+
+print("Just a couple more questions to setup your account.")
+print()
+
+age = input("What is your age? ")
+print()
+
+color = input("And what is your favorite color? ")
+print()
+
+print("Thank you", user_name, "for creating your account:")
+print("Name: ", user_name)
+print("Age: ", age)
+print("Favorite Color: ", color)
